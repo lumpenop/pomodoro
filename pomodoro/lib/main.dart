ae0d99bcaddd20343555ce475602352f3730e0d8
lumpenop <swk5373@gmail.com>
2023-04-23 00:03:52 +0900

    또운데이션

---
+++ pomodoro/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+            backgroundColor: const Color(0xFFE7626C),
+            textTheme:
+                const TextTheme(headline1: TextStyle(color: Color(0xFF232B55))),
+            cardColor: const Color(0XFFF4EDDB)),
+        home: const HomeScreen());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ pomodoro/lib/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Flexible(
+          flex: 1,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.red,
+            ),
+          ),
+        ),
+        Flexible(
+          flex: 2,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.green,
+            ),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
